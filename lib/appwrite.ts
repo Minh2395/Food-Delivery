dbcb355f84be7566f836a03e6e322d4ea17bc678
Minh2395 <vongominh2395@gmail.com>
2026-08-04 15:39:01 +0700

    Setup Appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.fooddelivery",
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+};
