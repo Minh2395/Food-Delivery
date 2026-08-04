753fd641cff1376a9d914de4b98f1b3bea730876
Minh2395 <vongominh2395@gmail.com>
2026-08-04 17:26:34 +0700

    Fixed Auth Screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.fooddelivery",
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: "user",
 };
